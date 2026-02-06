f53856c46b92044a2547be9b736f17ce86acae63
James Elsey <james@jameselsey.com>
2026-02-06 16:35:02 +0000

    Remove beta tag

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
     <a href="/" class="title">
-        <h1>GPX Route Splitter <sup class="beta">BETA</sup></h1>
+        <h1>GPX Route Splitter</h1>
         <img src="logo.png" alt="GPX Route Splitter" loading="eager">
     </a>
     <p class="intro">
