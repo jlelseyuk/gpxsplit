56a3ec0b280fc0a2daafe90f3f4534d152b99da2
James Elsey <james@jameselsey.com>
2026-02-15 12:21:14 +0000

    Add greeting to header

--- header.php
+++ header.php
@@ -4,6 +4,20 @@
         <img src="logo.png" alt="GPX Route Splitter" loading="eager">
     </a>
     <p class="intro">
+        <?php
+        date_default_timezone_set('Europe/London');
+        $hour = date("H");
+
+        if ($hour >= 2 && $hour < 12) {
+            $greeting = "Good morning! ";
+        } elseif ($hour >= 12 && $hour < 17) {
+            $greeting = "Good afternoon! ";
+        } else {
+            $greeting = "Good evening! ";
+        }
+
+        echo '<strong>' . $greeting . '</strong>';
+        ?>
         Upload your GPX file and choose up to 10 split points. The file will then be split into sequential parts,
         ready for individual download.
     </p>
