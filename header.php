00f85d42635878450025667c22a64727ca45adbb
James Elsey <james@jameselsey.com>
2026-02-15 17:08:06 +0000

    Remove greeting from header

--- header.php
+++ header.php
@@ -4,21 +4,7 @@
         <img src="logo.png" alt="GPX Route Splitter" loading="eager">
     </a>
     <p class="intro">
-        <?php
-        date_default_timezone_set('Europe/London');
-        $hour = date("H");
-
-        if ($hour >= 2 && $hour < 12) {
-            $greeting = "Good morning! ";
-        } elseif ($hour >= 12 && $hour < 17) {
-            $greeting = "Good afternoon! ";
-        } else {
-            $greeting = "Good evening! ";
-        }
-
-        echo '<strong>' . $greeting . '</strong>';
-        ?>
-        Upload your GPX file and choose up to 10 split points. The file will then be split into sequential parts,
+        Upload your GPX route file, then choose up to 10 split points. The route will then be split into sequential parts,
         ready for individual download.
     </p>
 </header>
